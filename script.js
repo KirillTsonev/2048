69813cbc27995d23f528626da8d935208997b037
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-09-16 21:53:54 +0300

    Third commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+///////////////
+// Variables //
+///////////////
+
+const container = document.querySelector(".container");
 let tiles = [...document.querySelectorAll(".wrapper__tile")];
-const outerTiles = document.querySelectorAll(".wrapper__tile-outer");
 
 const vertical1 = [...document.querySelectorAll(".vertical1")];
 const vertical2 = [...document.querySelectorAll(".vertical2")];
@@ -16,34 +20,39 @@ let scoreValue = 0;
 const highScore = document.querySelector(".header__highscore");
 let highScoreValue = 0;
 
-const newGame = document.querySelector(".header__button");
+const modalWinOverlay = document.querySelector(".modal__win__overlay");
+const modalLoseOverlay = document.querySelector(".modal__lose__overlay");
+const modalClose = document.querySelector(".modal__close");
+const modalWin = document.querySelector(".modal__win");
+const modalLose = document.querySelector(".modal__lose");
+const scoreModal = document.querySelector(".modal__score");
+
+const newGame = document.querySelector(".button-newGame");
+const continueGame = document.querySelector(".button-continueGame");
+const newGameNoConf = document.querySelectorAll(".button-newGameNoConf");
 
 let arrOne = [];
 let arrTwo = [];
 let arrThree = [];
+let result = [];
 
 const confirmation = document.createElement("div");
 confirmation.classList.add("header__confirmation");
 confirmation.textContent = "Are you sure?";
 
-newGame.addEventListener("click", () => {
-    newGame.replaceWith(confirmation);
-    setTimeout(() => {
-        confirmation.replaceWith(newGame);
-    }, 2500);
-});
+let checkVictoryResult = false;
+if (localStorage.getItem("checkVictoryResult")) {
+    checkVictoryResult = localStorage.getItem("checkVictoryResult");
+}
 
-confirmation.addEventListener("click", () => {
-    localStorage.removeItem("score");
-    scoreValue = 0;
-    score.textContent = scoreValue;
-    localStorage.removeItem("startPosition");
-    tiles.forEach(a => {
-        a.textContent = "";
-    });
-    firstInit();
-    confirmation.replaceWith(newGame);
-});
+let checkLossResult = false;
+if (localStorage.getItem("checkLossResult")) {
+    checkLossResult = localStorage.getItem("checkLossResult");
+}
+
+///////////////
+// Functions //
+///////////////
 
 function colors() {
     tiles.forEach(a => {
@@ -84,6 +93,9 @@ function colors() {
             case "2048":
                 a.style.background = "linear-gradient(90deg, rgba(254,255,0,1) 0%, rgba(255,0,0,1) 100%)";
                 break;
+            case "4096":
+                a.style.background = "linear-gradient(90deg, rgba(0,255,3,1) 0%, rgba(252,0,255,1) 100%)";
+                break;
         }
     });
 }
@@ -102,8 +114,6 @@ function checkPrevGame() {
         firstInit();
     }
 }
-
-checkPrevGame();
 
 function firstInit() {
     let random = Math.floor(Math.random() * 15);
@@ -143,7 +153,7 @@ function randomNumber() {
 
                 setTimeout(() => {
                     a.classList.remove("scaleNew");
-                }, 400);
+                }, 500);
             } else {
                 randomNumber();
             }
@@ -168,41 +178,22 @@ function setStartingPosition() {
     arrThree = [];
 }
 
-function moveLeft(a) {
+function moveUpLeft(a) {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             if (a[j].textContent === a[j + 1].textContent && a[j + 1].textContent.trim() !== "") {
                 a[j].textContent = a[j + 1].textContent * 2;
                 a[j + 1].textContent = "";
 
-                // a[j - 1].classList.add("scaleSum");
-                // setTimeout(() => {
-                //     a[j].classList.remove("scaleSum");
-                // }, 500);
+                a[j].classList.add("scaleSum");
+                setTimeout(() => {
+                    a[j].classList.remove("scaleSum");
+                }, 500);
 
                 scoreValue += +a[j].textContent;
                 score.textContent = `${scoreValue}`;
             } 
             else if (a[j].textContent.trim() === "") {
-                // setTimeout(() => {
-                    a[j].textContent = a[j + 1].textContent;
-                    a[j + 1].textContent = "";
-                // }, 4);
-            }
-        }
-    }
-}
-
-function moveUp(a) {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (a[j].textContent === a[j + 1].textContent && a[j + 1].textContent.trim() !== "") {
-                a[j].textContent = a[j + 1].textContent * 2;
-                a[j + 1].textContent = "";
-
-                scoreValue += +a[j].textContent;
-                score.textContent = `${scoreValue}`;
-            } else if (a[j].textContent.trim() === "") {
                 a[j].textContent = a[j + 1].textContent;
                 a[j + 1].textContent = "";
             }
@@ -210,29 +201,17 @@ function moveUp(a) {
     }
 }
 
-function moveDown(a) {
+function moveRightDown(a) {
     for (let i = 0; i < 3; i++) {
         for (let j = 3; j > 0; j--) {
             if (a[j].textContent === a[j - 1].textContent && a[j - 1].textContent.trim() !== "") {
                 a[j].textContent = a[j - 1].textContent * 2;
                 a[j - 1].textContent = "";
 
-                scoreValue += +a[j].textContent;
-                score.textContent = `${scoreValue}`;
-            } else if (a[j].textContent.trim() === "") {
-                a[j].textContent = a[j - 1].textContent;
-                a[j - 1].textContent = "";
-            }
-        }
-    }
-}
-
-function moveRight(a) {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 3; j > 0; j--) {
-            if (a[j].textContent === a[j - 1].textContent && a[j - 1].textContent.trim() !== "") {
-                a[j].textContent = a[j - 1].textContent * 2;
-                a[j - 1].textContent = "";
+                a[j].classList.add("scaleSum");
+                setTimeout(() => {
+                    a[j].classList.remove("scaleSum");
+                }, 500);
 
                 scoreValue += +a[j].textContent;
                 score.textContent = `${scoreValue}`;
@@ -245,31 +224,31 @@ function moveRight(a) {
 }
 
 function moveLeftAll() {
-    moveLeft(horizontal1);
-    moveLeft(horizontal2);
-    moveLeft(horizontal3);
-    moveLeft(horizontal4);
+    moveUpLeft(horizontal1);
+    moveUpLeft(horizontal2);
+    moveUpLeft(horizontal3);
+    moveUpLeft(horizontal4);
 }
 
 function moveUpAll() {
-    moveUp(vertical1);
-    moveUp(vertical2);
-    moveUp(vertical3);
-    moveUp(vertical4);
+    moveUpLeft(vertical1);
+    moveUpLeft(vertical2);
+    moveUpLeft(vertical3);
+    moveUpLeft(vertical4);
 }
 
 function moveRightAll() {
-    moveRight(horizontal1);
-    moveRight(horizontal2);
-    moveRight(horizontal3);
-    moveRight(horizontal4);
+    moveRightDown(horizontal1);
+    moveRightDown(horizontal2);
+    moveRightDown(horizontal3);
+    moveRightDown(horizontal4);
 }
 
 function moveDownAll() {
-    moveDown(vertical1);
-    moveDown(vertical2);
-    moveDown(vertical3);
-    moveDown(vertical4);
+    moveRightDown(vertical1);
+    moveRightDown(vertical2);
+    moveRightDown(vertical3);
+    moveRightDown(vertical4);
 }
 
 function recordOne() {
@@ -284,70 +263,216 @@ function recordTwo() {
     });
 }
 
+function playGame(moveFunc) {
+    recordOne();
+    moveFunc();
+    setScore();
+    recordTwo();
+    console.log(arrOne);
+    console.log(arrTwo);
+    determineLoss();
+    if (result.every(a => a === true) && tiles.every(a => a.textContent.trim() !== "")) {
+        loss();
+    }
+    if (JSON.stringify(arrOne) !== JSON.stringify(arrTwo)) {
+        randomNumber();
+    }
+    setStartingPosition();
+    colors();
+    arrOne = [];
+    arrTwo = [];
+    tiles.forEach(a => {
+        if (a.textContent === "2048") {
+            victory();
+        }
+    });
+}
+
+function showModal (selectorVictory, selectorOverlay) {
+    selectorVictory.classList.add("show");
+    selectorOverlay.classList.add("show");
+    selectorVictory.classList.remove("hide");
+    selectorOverlay.classList.remove("hide");
+    selectorVictory.classList.add("modalAppear");
+    selectorOverlay.classList.add("modalOverlayAppear");
+    setTimeout(() => {
+        selectorVictory.classList.remove("modalAppear");
+        selectorOverlay.classList.remove("modalOverlayAppear");
+    }, 2500);
+}
+
+function hideModal (selectorVictory, selectorOverlay) {
+    selectorVictory.classList.add("modalDisappear");
+    selectorOverlay.classList.add("modalOverlayDisappear");
+    setTimeout(() => {
+        selectorVictory.classList.remove("modalDisappear");
+        selectorOverlay.classList.remove("modalOverlayDisappear");
+        selectorVictory.classList.remove("show");
+        selectorOverlay.classList.remove("show");
+        selectorVictory.classList.add("hide");
+        selectorOverlay.classList.add("hide");
+    }, 2000);
+}
+
+function modalCloseTriggers(selectorVictory, selectorOverlay) {
+    selectorOverlay.addEventListener("click", (e) => {
+        if (e.target === selectorOverlay || e.target.getAttribute("data-close") === "") {
+            hideModal(selectorVictory, selectorOverlay);
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code === "Escape" && !selectorVictory.classList.contains("hide")) {
+            hideModal(selectorVictory, selectorOverlay);
+        }
+    });
+}
+
+function victory() {
+    if (checkVictoryResult === false) {
+        setTimeout(() => {
+            showModal(modalWin, modalWinOverlay);
+            modalCloseTriggers(modalWin, modalWinOverlay);
+            scoreModal.textContent = scoreValue;
+        }, 500);
+    }
+    checkVictoryResult = true;
+    localStorage.setItem("checkVictoryResult", checkVictoryResult);
+}
+
+function determineLoss() {
+    for (let i = 0; i < 3; i++) {
+        if (horizontal1[i] !== horizontal1[i + 1]) {
+            result.push(true);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (horizontal2[i] !== horizontal2[i + 1]) {
+            result.push(true);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (horizontal3[i] !== horizontal3[i + 1]) {
+            result.push(true);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (horizontal4[i] !== horizontal4[i + 1]) {
+            result.push(true);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (vertical1[i] !== vertical1[i + 1]) {
+            result.push(true);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (vertical2[i] !== vertical2[i + 1]) {
+            result.push(true);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (vertical3[i] !== vertical3[i + 1]) {
+            result.push(true);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (vertical4[i] !== vertical4[i + 1]) {
+            result.push(true);
+        }
+    }
+}
+
+function loss() {
+    if (checkLossResult === false) {
+        setTimeout(() => {
+            showModal(modalLose, modalLoseOverlay);
+            modalCloseTriggers(modalLose, modalLoseOverlay);
+            scoreModal.textContent = scoreValue;
+        }, 500);
+    }
+    checkLossResult = true;
+}
+
+/////////////////////
+// Event Listeners //
+/////////////////////
+
+newGame.addEventListener("click", () => {
+    newGame.replaceWith(confirmation);
+    container.addEventListener("click", (e) => {
+        if (e.target !== newGame) {
+            confirmation.replaceWith(newGame);
+        }
+    });
+    setTimeout(() => {
+        confirmation.replaceWith(newGame);
+    }, 1300);
+});
+
+confirmation.addEventListener("click", () => {
+    localStorage.removeItem("checkVictoryResult");
+    localStorage.removeItem("score");
+    localStorage.removeItem("startPosition");
+    checkVictoryResult = false;
+    checkLossResult = false;
+    scoreValue = 0;
+    score.textContent = scoreValue;
+    tiles.forEach(a => {
+        a.textContent = "";
+    });
+    firstInit();
+    confirmation.replaceWith(newGame);
+});
+
+newGameNoConf.forEach(a => {
+    a.addEventListener("click", () => {
+        localStorage.removeItem("checkVictoryResult");
+        localStorage.removeItem("score");
+        localStorage.removeItem("startPosition");
+        checkVictoryResult = false;
+        scoreValue = 0;
+        score.textContent = scoreValue;
+        tiles.forEach(a => {
+            a.textContent = "";
+        });
+        firstInit();
+        hideModal(modalWin, modalWinOverlay);
+        hideModal(modalLose, modalLoseOverlay);
+    });
+});
+
+continueGame.addEventListener("click", () => {
+    hideModal(modalWin, modalWinOverlay);
+    modalCloseTriggers(modalWin, modalWinOverlay);
+});
+
 document.addEventListener("keydown", (e) => {
     if (e.code === "ArrowLeft") {
-        recordOne();
-        moveLeftAll();
-        setScore();
-        recordTwo();
-        if (JSON.stringify(arrOne) !== JSON.stringify(arrTwo)) {
-            randomNumber();
-        }
-        setStartingPosition();
-        colors();
-        arrOne = [];
-        arrTwo = [];
+        playGame(moveLeftAll);
     }
 });
 
 document.addEventListener("keydown", (e) => {
     if (e.code === "ArrowRight") {
-        recordOne();
-        moveRightAll();
-        setScore();
-        recordTwo();
-        if (JSON.stringify(arrOne) !== JSON.stringify(arrTwo)) {
-            randomNumber();
-        }
-        setStartingPosition();
-        colors();
-        arrOne = [];
-        arrTwo = [];
+        playGame(moveRightAll);
     }
 });
 
 document.addEventListener("keydown", (e) => {
     if (e.code === "ArrowUp") {
-        recordOne();
-        moveUpAll();
-        setScore();
-        recordTwo();
-        if (JSON.stringify(arrOne) !== JSON.stringify(arrTwo)) {
-            randomNumber();
-        }
-        setStartingPosition();
-        colors();
-        arrOne = [];
-        arrTwo = [];
+        playGame(moveUpAll);
     }
 });
 
 document.addEventListener("keydown", (e) => {
     if (e.code === "ArrowDown") {
-        recordOne();
-        moveDownAll();
-        setScore();
-        recordTwo();
-        if (JSON.stringify(arrOne) !== JSON.stringify(arrTwo)) {
-            randomNumber();
-        }
-        setStartingPosition();
-        colors();
-        arrOne = [];
-        arrTwo = [];
+        playGame(moveDownAll);
     }
 });
 
-//animations
-//results
-//flare
+////////////////////
+// Function Calls //
+////////////////////
+
+checkPrevGame();
